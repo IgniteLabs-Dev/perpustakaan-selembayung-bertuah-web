17c161b18d6846af96971505ccc98a6444d4fa2e
Wahyu NT <wahyuntx28@gmail.com>
2025-03-12 15:49:15 +0700

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>App Name - @yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
