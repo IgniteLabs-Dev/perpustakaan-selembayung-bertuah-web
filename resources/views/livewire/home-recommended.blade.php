1f23438d5f065adef7cf10c961481b654907161a
Wahyu NT <wahyuntx28@gmail.com>
2025-03-28 04:47:21 +0700

    Enhance HomeRecommended component: update title color and wrap book images in links for improved navigation

--- resources/views/livewire/home-recommended.blade.php
+++ resources/views/livewire/home-recommended.blade.php
@@ -1,11 +1,14 @@
 <div>
     <div class="w-full bg-stripes">
         <div class="max-w-screen-xl mx-auto">
-            <h1 class="text-2xl font-bold text-center text-black pt-20 ">Rekomendasi Buku</h1>
+            <h1 class="text-2xl font-bold text-center text-[var(--primary)] pt-20 ">Rekomendasi Buku</h1>
             <div class="flex justify-center items-end space-x-6 pt-5 pb-25">
                 @forelse ($books as $index => $book)
-                    <img class="{{ $heights[$index % count($heights)] }} rounded-md"
-                        src="{{ asset('images/books/' . $book->cover) }}" alt="">
+                    <a href="{{ route('detail-buku', ['id' => $book->id]) }}">
+
+                        <img class="{{ $heights[$index % count($heights)] }} hover:scale-110 hover:shadow-md transition-transform duration-200 ease-in-out   rounded-md"
+                            src="{{ asset('images/books/' . $book->cover) }}" alt="">
+                    </a>
                 @empty
                 @endforelse
 
