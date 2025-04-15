b854b3b88fc33bbf9dd6987d822c4b3d36966e3c
Wahyu NT <wahyuntx28@gmail.com>
2025-04-15 22:14:07 +0700

    Refactor dashboard layout: adjust grid structure and ensure consistent height for statistics cards

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -40,29 +40,29 @@
 
         </div>
     </div> --}}
-    <div class="grid max-w-screen-xl grid-cols-2 gap-4 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6  sm:p-8">
+    <div class="grid  grid-cols-3 gap-4 p-4  text-gray-900 3 ">
 
-        <div class="flex flex-col items-center justify-center   rounded-lg shadow-sm bg-white p-4">
+        <div class="flex flex-col h-36 py-5 items-center justify-center   rounded-lg shadow-sm bg-white p-4">
             <dt class="mb-2 text-3xl font-extrabold">{{ $visitor }}</dt>
             <dd class="text-gray-500 ">Pengunjung</dd>
         </div>
-        <div class="flex flex-col items-center justify-center   rounded-lg shadow-sm bg-white p-4">
+        <div class="flex flex-col h-36 py-5 items-center justify-center   rounded-lg shadow-sm bg-white p-4">
             <dt class="mb-2 text-3xl font-extrabold">{{ $book }}</dt>
             <dd class="text-gray-500 ">Judul Buku</dd>
         </div>
-        <div class="flex flex-col items-center justify-center   rounded-lg shadow-sm bg-white p-4">
+        <div class="flex flex-col h-36 py-5 items-center justify-center   rounded-lg shadow-sm bg-white p-4">
             <dt class="mb-2 text-3xl font-extrabold">{{ $bookStock }}</dt>
             <dd class="text-gray-500 ">Stok Buku Tersedia</dd>
         </div>
-        <div class="flex flex-col items-center justify-center   rounded-lg shadow-sm bg-white p-4">
+        <div class="flex flex-col h-36 py-5 items-center justify-center   rounded-lg shadow-sm bg-white p-4">
             <dt class="mb-2 text-3xl font-extrabold">{{ $author }}</dt>
             <dd class="text-gray-500 ">Penulis</dd>
         </div>
-        <div class="flex flex-col items-center justify-center   rounded-lg shadow-sm bg-white p-4">
+        <div class="flex flex-col h-36 py-5 items-center justify-center   rounded-lg shadow-sm bg-white p-4">
             <dt class="mb-2 text-3xl font-extrabold">{{ $category }}</dt>
             <dd class="text-gray-500 ">Kategori</dd>
         </div>
-        <div class="flex flex-col items-center justify-center   rounded-lg shadow-sm bg-white p-4">
+        <div class="flex flex-col h-36 py-5 items-center justify-center   rounded-lg shadow-sm bg-white p-4">
             <dt class="mb-2 text-3xl font-extrabold">{{ $loan }}</dt>
             <dd class="text-gray-500 ">Buku Dipinjam</dd>
         </div>
