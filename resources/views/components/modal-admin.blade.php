811a9a159a5814d4b7c5b06ff739fa556dc475b6
Wahyu NT <wahyuntx28@gmail.com>
2025-04-18 17:59:24 +0700

    Refactor modal-admin component: update modal background color, improve icon usage for navigation links, and enhance overall accessibility.

--- resources/views/components/modal-admin.blade.php
+++ resources/views/components/modal-admin.blade.php
@@ -1,12 +1,15 @@
 <!-- Main modal -->
 <div id="authentication-modal" tabindex="-1" aria-hidden="true"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ">
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black/50">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow-sm ">
             <!-- Modal header -->
-            <div class="flex items-center justify-center p-1 md:p-5 border-b rounded-t  border-gray-200">
-                <img class="h-[80px]" src="{{ asset('images/Perpustakaan Icon.png') }}" alt="">
+            <div class="flex items-center justify-center px-1 py-2 md:p-5 border-b rounded-t  border-gray-200">
+                <a href="/" class="cursor-pointer">
+
+                    <img class="h-[80px]" src="{{ asset('images/Perpustakaan Icon.png') }}" alt="">
+                </a>
             </div>
             <!-- Modal body -->
             <div class="px-4">
@@ -19,7 +22,7 @@
                                 class="flex items-center ps-5 px-4 py-2 rounded-md   
                             {{ request()->routeIs('dashboard') ? 'bg-blue-100 text-blue-500 text-lg  ' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                                <i class='bx bx-layer'></i>
+                                <i class="fa-solid fa-home"></i>
                                 <span class="ms-2">Dashboard</span>
                             </a>
                         </a>
@@ -32,7 +35,7 @@
                                 class="flex items-center ps-5 px-4 py-2 rounded-md   
                             {{ request()->routeIs('admin-manajemen-user') ? 'bg-blue-100 text-blue-500 text-lg  ' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                                <i class='bx bx-layer'></i>
+                                <i class="fa-solid fa-user"></i>
                                 <span class="ms-2">Manajemen User</span>
                             </a>
                         </a>
@@ -45,7 +48,7 @@
                                 class="flex items-center ps-5 px-4 py-2 rounded-md   
                             {{ request()->routeIs('admin-manajemen-buku') ? 'bg-blue-100 text-blue-500 text-lg  ' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                                <i class='bx bx-layer'></i>
+                                <i class="fa-solid fa-book"></i>
                                 <span class="ms-2">Manajemen Buku</span>
                             </a>
                         </a>
@@ -58,7 +61,7 @@
                                 class="flex items-center ps-5 px-4 py-2 rounded-md   
                             {{ request()->routeIs('admin-manajemen-penulis') ? 'bg-blue-100 text-blue-500 text-lg  ' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                                <i class='bx bx-layer'></i>
+                                <i class="fa-solid fa-feather-pointed"></i>
                                 <span class="ms-2">Manajemen Penulis</span>
                             </a>
                         </a>
@@ -71,7 +74,7 @@
                                 class="flex items-center ps-5 px-4 py-2 rounded-md   
                             {{ request()->routeIs('admin-manajemen-kategori') ? 'bg-blue-100 text-blue-500 text-lg  ' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                                <i class='bx bx-layer'></i>
+                                <i class="fa-solid fa-tags"></i>
                                 <span class="ms-2">Manajemen Kategori</span>
                             </a>
                         </a>
@@ -84,7 +87,7 @@
                                 class="flex items-center ps-5 px-4 py-2 rounded-md   
                             {{ request()->routeIs('admin-manajemen-peminjaman') ? 'bg-blue-100 text-blue-500 text-lg  ' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                                <i class='bx bx-layer'></i>
+                                <i class="fa-solid fa-address-book"></i>
                                 <span class="ms-2">Manajemen Peminjaman</span>
                             </a>
                         </a>
@@ -97,7 +100,7 @@
                                 class="flex items-center ps-5 px-4 py-2 rounded-md   
                             {{ request()->routeIs('admin-manajemen-reward') ? 'bg-blue-100 text-blue-500 text-lg  ' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                                <i class='bx bx-layer'></i>
+                                <i class="fa-solid fa-medal"></i>
                                 <span class="ms-2">Manajemen Reward</span>
                             </a>
                         </a>
@@ -107,7 +110,7 @@
                             class="mb-3 flex items-center ps-5 px-4  py-2 rounded-md w-full 
                             {{ request()->routeIs('admin-manajemen-pengunjung') ? 'bg-blue-100 text-blue-500 text-lg' : 'bg-transparent text-gray-700 border-r-0' }}
                             hover:bg-blue-200">
-                            <i class='bx bx-layer'></i>
+                            <i class="fa-solid fa-user-secret   "></i>
                             <span class="ms-2">Manajemen Pengunjung</span>
                         </a>
                     </div>
