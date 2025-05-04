5b4187b3739d818271382b5440c54b5e44d687c7
Wahyu NT <wahyuntx28@gmail.com>
2025-05-04 20:00:57 +0700

    Make 'cover' field nullable in users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('cover')->unique();
+            $table->string('cover')->nullable();
             $table->string('name');
             $table->string('nis')->unique()->nullable();
             $table->string('email')->unique();
