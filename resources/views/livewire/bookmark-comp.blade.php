bda5e61152891d33b7cd07c4e74e33336e342fdf
Wahyu NT <wahyuntx28@gmail.com>
2025-06-03 13:45:07 +0700

    update bookmark

--- resources/views/livewire/bookmark-comp.blade.php
+++ resources/views/livewire/bookmark-comp.blade.php
@@ -24,63 +24,67 @@
                     </div>
 
                     <div class="div">
+                        @if ($data->isEmpty())
+                            <div class="">
+                            @else
+                                <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2  gap-2">
+                        @endif
                         @forelse($data as $item)
-                            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2  gap-2">
+                            <div class="w-full mb-0 h-full">
 
-                                <div class="w-full mb-0 h-full">
+                                <div class="border-gray-300 p-3 border flex rounded-lg justify-start">
+                                    <div class="flex  ">
+                                        <div class="w-3/7">
 
-                                    <div class="border-gray-300 p-3 border flex rounded-lg justify-start">
-                                        <div class="flex  ">
-                                            <div class="w-3/7">
-
-                                                <a class="h-full"
-                                                    href="{{ route('detail-buku', ['id' => $item->book->id]) }}">
-                                                    <img class="w-36  hover:scale-102 transition-transform duration-300 ease-in-out object-cover rounded-lg  "
-                                                        src="{{ asset('images/books/' . $item->book->cover) }}"
-                                                        alt="">
-                                                </a>
-                                            </div>
-                                            <div class="ms-2 flex w-4/7 flex-col ">
-                                                <a href="{{ route('detail-buku', ['id' => $item->book->id]) }}">
-                                                    <div class="flex flex-col flex-wrap">
-
-                                                        <p
-                                                            class="text-xs mb-0  text-gray-700 overflow-hidden text-ellipsis hover:whitespace-normal whitespace-nowrap max-w-[150px]">
-                                                            {{ $item->authors }}
-                                                        </p>
-
-                                                        <h1
-                                                            class="text-md font-bold mb-1 mt-0 whitespace- text-black leading-tight">
-                                                            {{ $item->book->title }} </h1>
-                                                        <p class="text-xs  text-black">Stok :
-                                                            {{ $item->book->stock ?? '' }}</p>
-                                                    </div>
-                                                </a>
-
-                                                <button wire:click="removeBookmark({{ $item->book_id }})"
-                                                    class=" cursor-pointer mt-auto hover:scale-105 transition-transform duration-300 ease-in-out rounded-full outline-1 outline-red-700 text-red-700  hover:bg-red-700  hover:text-white hover:shadow-sm focus:ring-0 focus:outline-1 focus:ring-gray-300 font-medium text-xs px-2 py-1 active:outline-0 active:outline-none">Hapus
-                                                    dari
-                                                    Favorit</button>
-
-
-                                            </div>
+                                            <a class="h-full"
+                                                href="{{ route('detail-buku', ['id' => $item->book->id]) }}">
+                                                <img class="w-36  hover:scale-102 transition-transform duration-300 ease-in-out object-cover rounded-lg  "
+                                                    src="{{ asset('images/books/' . $item->book->cover) }}"
+                                                    alt="">
+                                            </a>
                                         </div>
+                                        <div class="ms-2 flex w-4/7 flex-col ">
+                                            <a href="{{ route('detail-buku', ['id' => $item->book->id]) }}">
+                                                <div class="flex flex-col flex-wrap">
 
+                                                    <p
+                                                        class="text-xs mb-0  text-gray-700 overflow-hidden text-ellipsis hover:whitespace-normal whitespace-nowrap max-w-[150px]">
+                                                        {{ $item->authors }}
+                                                    </p>
+
+                                                    <h1
+                                                        class="text-md font-bold mb-1 mt-0 whitespace- text-black leading-tight">
+                                                        {{ $item->book->title }} </h1>
+                                                    <p class="text-xs  text-black">Stok :
+                                                        {{ $item->book->stock ?? '' }}</p>
+                                                </div>
+                                            </a>
+
+                                            <button wire:click="removeBookmark({{ $item->book_id }})"
+                                                class=" cursor-pointer mt-auto hover:scale-105 transition-transform duration-300 ease-in-out rounded-full outline-1 outline-red-700 text-red-700  hover:bg-red-700  hover:text-white hover:shadow-sm focus:ring-0 focus:outline-1 focus:ring-gray-300 font-medium text-xs px-2 py-1 active:outline-0 active:outline-none">Hapus
+                                                dari
+                                                Favorit</button>
+
+
+                                        </div>
                                     </div>
 
                                 </div>
+
                             </div>
+
                         @empty
                             <div class="w-full text-center justify-center flex items-center">
                                 <p class="text-md font-semibold  text-gray-600">Buku Favorit Tidak Ada</p>
                             </div>
                         @endforelse
                     </div>
-
                 </div>
+
             </div>
         </div>
-
     </div>
+
+</div>
 
 </div>
